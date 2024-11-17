28587b29aaab10c78a6b70576e32be727f4feda0
Abhinavks1405 <abhinavkumarsingh2023@gmail.com>
2024-11-17 12:59:16 +0530

    updated Hero.tsx , added grain and rings

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,19 @@
-import memojiImage from "@assets/images/memoji-computer.png";
+import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
-import ArrowDown from '@assets/icons/arrow-down.svg';
+import ArrowDown from '@/assets/icons/arrow-down.svg';
+import GrainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
 
 export const HeroSection = () => {
   return (
-  <div className="py-32 md:py-48 lg:py-60">
+  <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div className="absolute inset-0 -z-30 opacity-5" 
+         style={{ backgroundImage: `url(${GrainImage.src})` }}>
+      </div>
+      <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
     <div className="container">
       <div className="flex flex-col items-center">
         <Image 
@@ -17,14 +26,14 @@ export const HeroSection = () => {
           <div className="text-sm font-medium">Avaliable for New Project</div>
       </div>
     </div>
-    <div className="'max-w-lg mx-auto">
+    <div className="max-w-lg mx-auto">
     <h1 className="font-serif text-3xl md:text-5xl text center mt-8 tracking-wide font-semibold text-center ">
       Learning Machine Learning
     </h1>
       <p className="mt-4 text-center text-white/60 md:text-lg">
-      Aspiring ML Engineer skilled in Python for data manipulation and visualization. 
-      Actively exploring Deep Learning and Natural Language Processing. 
-      Committed to advancing AI through continuous learning. 
+        Aspiring ML Engineer skilled in Python for data manipulation and visualization. 
+        Actively exploring Deep Learning and Natural Language Processing. 
+        Committed to advancing AI through continuous learning. 
       </p>
     </div>
     <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
