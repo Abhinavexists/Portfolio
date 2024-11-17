bc58fddc0b0fc2325a1ea82667d5f176eb016c34
Abhinavks1405 <abhinavkumarsingh2023@gmail.com>
2024-11-17 21:07:15 +0530

    Added Orbit.tsx , Element added to hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import ArrowDown from '@/assets/icons/arrow-down.svg';
 import GrainImage from "@/assets/images/grain.jpg";
 import StarIcon from "@/assets/icons/star.svg";
+import { Orbit } from "@/components/Orbit";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
 
 export const HeroSection = () => {
   return (
@@ -10,10 +12,37 @@ export const HeroSection = () => {
       <div className="absolute inset-0 -z-30 opacity-5" 
          style={{ backgroundImage: `url(${GrainImage.src})` }}>
       </div>
-      <div className="size-[620px] hero-ring"></div>
-      <div className="size-[820px] hero-ring"></div>
-      <div className="size-[1020px] hero-ring"></div>
-      <div className="size-[1220px] hero-ring"></div>
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
+      <Orbit size={800} rotation={-72}>
+        <StarIcon className="size-20 text-pink-300"/>
+      </Orbit>
+      <Orbit size={550} rotation={20}>
+        <StarIcon className="size-12 text-pink-300"/>
+      </Orbit>
+      <Orbit size={590} rotation={100}>
+        <StarIcon className="size-8 text-pink-300"/>
+      </Orbit>
+      <Orbit size={420} rotation={-15}>
+        <SparkleIcon className="size-8 text-pink-300/20"/>
+      </Orbit>
+      <Orbit size={440} rotation={178}>
+        <SparkleIcon className="size-10 text-pink-300/20"/>
+      </Orbit>
+      <Orbit size={710} rotation={145}>
+        <SparkleIcon className="size-14 text-pink-300/20"/>
+      </Orbit>
+      <Orbit size={720} rotation={90}>
+        <div className="size-3 rounded-full bg-pink-300/20"/>
+      </Orbit>
+      <Orbit size={520} rotation={-40}>
+        <div className="size-2 rounded-full bg-pink-300/20"/>
+      </Orbit>
+      <Orbit size={650} rotation={-5}>
+        <div className="size-2 rounded-full bg-pink-300/20"/>
+      </Orbit>
     <div className="container">
       <div className="flex flex-col items-center">
         <Image 
