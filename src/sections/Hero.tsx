06e24d9959479134ead51265a336691dbfd4f476
Abhinavks1405 <abhinavkumarsingh2023@gmail.com>
2024-11-21 05:02:29 +0530

    Updated Orbit.tsx , Hero.tsx , added rotatation Animation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,7 +9,7 @@ import SparkleIcon from "@/assets/icons/sparkle.svg";
 export const HeroSection = () => {
   return (
   <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-    <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_70%,transparent">
+    <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_90%,transparent)]">
         <div className="absolute inset-0 -z-30 opacity-5" 
           style={{ backgroundImage: `url(${GrainImage.src})` }}>
         </div>
@@ -17,33 +17,33 @@ export const HeroSection = () => {
           <div className="size-[820px] hero-ring"></div>
           <div className="size-[1020px] hero-ring"></div>
           <div className="size-[1220px] hero-ring"></div>
-        <Orbit size={800} rotation={-72}>
-          <StarIcon className="size-20 text-pink-300"/>
-        </Orbit>
-        <Orbit size={550} rotation={20}>
-          <StarIcon className="size-12 text-pink-300"/>
-        </Orbit>
-        <Orbit size={590} rotation={100}>
-          <StarIcon className="size-8 text-pink-300"/>
-        </Orbit>
-        <Orbit size={420} rotation={-15}>
-          <SparkleIcon className="size-8 text-pink-300/20"/>
-        </Orbit>
-        <Orbit size={440} rotation={178}>
-          <SparkleIcon className="size-10 text-pink-300/20"/>
-        </Orbit>
-        <Orbit size={710} rotation={145}>
-          <SparkleIcon className="size-14 text-pink-300/20"/>
-        </Orbit>
-        <Orbit size={720} rotation={90}>
-          <div className="size-3 rounded-full bg-pink-300/20"/>
-        </Orbit>
-        <Orbit size={520} rotation={-40}>
-          <div className="size-2 rounded-full bg-pink-300/20"/>
-        </Orbit>
-        <Orbit size={650} rotation={-5}>
-          <div className="size-2 rounded-full bg-pink-300/20"/>
-        </Orbit>
+            <Orbit size={800} rotation={-72} shouldOrbit orbitDuration="42s" shouldSpin spinDuration="15s">
+            <StarIcon className="size-20 text-pink-300"/>
+            </Orbit>
+            <Orbit size={550} rotation={20} shouldOrbit orbitDuration="37s" shouldSpin spinDuration="20s"> {/* Slow spin */}
+              <StarIcon className="size-12 text-pink-300"/>
+            </Orbit>
+            <Orbit size={590} rotation={100} shouldOrbit orbitDuration="33s" shouldSpin spinDuration="18s"> {/* Moderate spin */}
+              <StarIcon className="size-8 text-pink-300" />
+            </Orbit>
+            <Orbit size={420} rotation={-15} shouldOrbit orbitDuration="28s" shouldSpin spinDuration="10s"> {/* Faster spin */}
+              <SparkleIcon className="size-8 text-pink-300/20"/>
+            </Orbit>
+            <Orbit size={440} rotation={178} shouldOrbit orbitDuration="31s" shouldSpin spinDuration="12s"> {/* Balanced spin */}
+              <SparkleIcon className="size-10 text-pink-300/20"/>
+            </Orbit>
+            <Orbit size={710} rotation={145} shouldOrbit orbitDuration="46s" shouldSpin spinDuration="25s"> {/* Slow spin */}
+              <SparkleIcon className="size-14 text-pink-300/20"/>
+            </Orbit>
+            <Orbit size={720} rotation={90} shouldOrbit orbitDuration="50s" shouldSpin spinDuration="30s"> {/* Very slow spin */}
+              <div className="size-3 rounded-full bg-pink-300/20"/>
+            </Orbit>
+            <Orbit size={520} rotation={-40} shouldOrbit orbitDuration="29s" shouldSpin spinDuration="14s"> {/* Dynamic spin */}
+              <div className="size-2 rounded-full bg-pink-300/20"/>
+            </Orbit>
+            <Orbit size={650} rotation={-5} shouldOrbit orbitDuration="44s" shouldSpin spinDuration="22s"> {/* Smooth spin */}
+              <div className="size-2 rounded-full bg-pink-300/20"/>
+            </Orbit>
       </div>
     <div className="container">
       <div className="flex flex-col items-center">
@@ -53,7 +53,9 @@ export const HeroSection = () => {
           alt="Person Peeking"
           />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="bg-green-500 size-2.5 rounded-full relative">
+              <div className="bg-green-500 absolute inset-0 rounded-full animate-ping "></div>
+            </div>
           <div className="text-sm font-medium">Avaliable for New Project</div>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   		lg: '1200px'
   	},
   	container: {
-  		center: 'true',
+		center: true,
   		padding: {
   			DEFAULT: '1rem',
   			md: '2rem'
@@ -72,8 +72,19 @@ const config: Config = {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
-  		}
-  	}
+  		},
+		animation: {
+			'ping-large': "ping large ls ease-in-out infinite"
+		},
+		keyframes: {
+			'ping-large': {
+				'75%, 100%': {
+					transform: 'scale(3)',
+					opacity: '0',
+				}
+			}
+		}
+	},
   },
   plugins: [require("tailwindcss-animate")],
 };
