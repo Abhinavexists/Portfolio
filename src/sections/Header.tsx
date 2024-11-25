7a6fe78ce1c509ae758ad3a6504e57c452996bb7
Abhinavks1405 <abhinavkumarsingh2023@gmail.com>
2024-11-25 21:48:28 +0530

    Added Profile photo and resume button

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,21 +4,36 @@ import { smoothScroll } from "@/components/smoothScroll";
 
 export const Header = () => {
   return (
-    <header className="flex justify-center items-center fixed top-3 w-full z-10">
-      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur-xl">
-        <a href="#home" className="nav-item" onClick={smoothScroll}>
-          Home
-        </a>
-        <a href="#projects" className="nav-item" onClick={smoothScroll}>
-          Projects
-        </a>
-        <a href="#about" className="nav-item" onClick={smoothScroll}>
-          About
-        </a>
-        <a href="#contact" className="nav-item" onClick={smoothScroll}>
-          Contact
-        </a>
-      </nav>
+    <header className="flex justify-between items-center fixed top-3 w-full z-10 px-4">
+      <div className="flex-1"></div>
+      <div className="flex-1 flex justify-center">
+        <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur-xl">
+          <a href="#home" className="nav-item" onClick={smoothScroll}>
+            Home
+          </a>
+          <a href="#projects" className="nav-item" onClick={smoothScroll}>
+            Projects
+          </a>
+          <a href="#about" className="nav-item" onClick={smoothScroll}>
+            About
+          </a>
+          <a href="#contact" className="nav-item" onClick={smoothScroll}>
+            Contact
+          </a>
+        </nav>
+      </div>
+      <div className="flex-1 flex justify-end items-center">
+        <nav className="p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur-xl">
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="nav-item"
+          >
+            Resume
+          </a>
+        </nav>
+      </div>
     </header>
   );
 };
